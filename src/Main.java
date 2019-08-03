6faf9760f01f2eb0691a55b05c7cb01ce084d3f8
seohappy <seoha_ppy@naver.com>
2019-08-03 23:48:00 +0900

    Static

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,13 @@
 
 public class Main {
+	
+	final static double PI = 3.141592;
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		int r = 30;
+		System.out.println(r * r * PI);
 		
-		int intType = 100;
-		double doubleType = 150.5;
-		String stringType = "¿Ãº≠«œ";
-		
-		System.out.println(intType);
-		System.out.println(doubleType);
-		System.out.println(stringType);
 	}
 
 }
