c8f025decffc58aa74e0150c3492bfbc952a8839
seohappy <seoha_ppy@naver.com>
2019-08-03 23:55:45 +0900

    Calculation

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,16 @@
 
 public class Main {
 	
-	final static int INT_MAX = 2147483647;
 	
 	public static void main(String[] args) {
 
-		int a = INT_MAX;
-		System.out.println(a + 1);
-		//Overflow
+		int a = 1;
+		int b = 2;
 		
+		System.out.println("a + b = " + (a + b));
+		System.out.println("a - b = " + (a - b));
+		System.out.println("a * b = " + (a * b));
+		System.out.println("a / b = " + (a / b));
 	}
 
 }
