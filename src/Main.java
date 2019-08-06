2123e2ccc2a41b475f1248b1aa7c5cd6654aeb61
seohappy <seoha_ppy@naver.com>
2019-08-06 13:20:51 +0900

    ㅁㅍㄷㄱㅁㅎㄷ

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = (int) 0.5; //버림
+		System.out.println(a);
+		double b = 0.5;
+		a = (int) (b+0.5); //반올림
+		System.out.println(a);
+		
+		///////////자료형/////////////
+		
+	}
+
+}
