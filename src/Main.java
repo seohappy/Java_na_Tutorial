549cd11841057b8ddbc2211a09acd1d4cdec14c6
seohappy <seoha_ppy@naver.com>
2019-08-06 13:57:15 +0900

    Decimal/Octal'Hexadecimal Number

--- src/Main.java
+++ src/Main.java
@@ -1,44 +1,13 @@
 
 public class Main {
-<<<<<<< HEAD
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int a = (int) 0.5; //버림
-		System.out.println(a);
-		double b = 0.5;
-		a = (int) (b+0.5); //반올림
-		System.out.println(a);
-		
-		///////////자료형/////////////
-		
-=======
 	
-	
-	public static void main(String[] args) {
+		int a = 200;
 		
-<<<<<<< HEAD
-		double a = 10.3;
-		double b = 9.6;
-		double c = 10.1;
-		
-		System.out.println((a+b+c)/3); //평균값 구하기
-		
-		for (char i = 'a'; i<= 'z'; i++)
-		{
-			System.out.print(i+" ");
-		}
-		//아스키 코드로 알파벳 출력
-		
-		
-
-=======
-		System.out.println("a + b = " + (a + b));
-		System.out.println("a - b = " + (a - b));
-		System.out.println("a * b = " + (a * b));
-		System.out.println("a / b = " + (a / b));
->>>>>>> refs/remotes/origin/master
->>>>>>> refs/remotes/origin/master
+		System.out.println("10진수: " + a);
+		System.out.format("8진수: %o\n", a);
+		System.out.format("16진수: %x", a);
 	}
-
+	
 }
